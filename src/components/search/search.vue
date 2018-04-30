edbc32228a11d0b342a47ff1211f4105ff400bb7
liuyongqi <529169579@qq.com>
2018-04-30 17:35:17 +0800

    first commit

---
+++ src/components/search/search.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="search">
+		你好  search
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+	.search
+		font-size:12px
+</style>
